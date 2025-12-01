147ce94ff19905081e1995f977ecc440da6f5363
Michelle Gomes <gmichele498@gmail.com>
2025-11-30 23:49:39 -0300

    Replace WebView with Native Android implementation

---
+++ app/src/main/java/br/edu/ifsudestemg/reignsia/CardAdapter.java
@@ -0,0 +1,44 @@
+package br.edu.ifsudestemg.reignsia;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class CardAdapter extends RecyclerView.Adapter<CardAdapter.ViewHolder> {
+
+    private List<String> cards;
+
+    public CardAdapter(List<String> cards) {
+        this.cards = cards;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_card, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText(cards.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return cards.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        ViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.card_text);
+        }
+    }
+}
