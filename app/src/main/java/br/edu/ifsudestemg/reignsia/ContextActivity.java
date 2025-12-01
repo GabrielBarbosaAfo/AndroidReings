7a00be2043ea3cc643b90db9d01bf577d1246376
Michelle Gomes <gmichele498@gmail.com>
2025-11-30 21:55:59 -0300

    design initial app

---
+++ app/src/main/java/br/edu/ifsudestemg/reignsia/ContextActivity.java
@@ -0,0 +1,37 @@
+package br.edu.ifsudestemg.reignsia;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContextActivity extends AppCompatActivity {
+
+    private Button startButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_context);
+        initViews();
+        setupClick();
+    }
+
+    private void initViews() {
+        startButton = findViewById(R.id.btn_start);
+    }
+
+    private void setupClick() {
+        startButton.setOnClickListener(v -> {
+            goToGame();
+        });
+    }
+
+    private void goToGame() {
+        startButton.setOnClickListener(v -> startActivity(new Intent(this, GameActivity.class)));
+        Intent intent = new Intent(this, GameActivity.class);
+
+    }
+}
---
+++ app/src/main/java/br/edu/ifsudestemg/reignsia/SplashActivity.java
@@ -0,0 +1,61 @@
+package br.edu.ifsudestemg.reignsia;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+    private View fadeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        videoView = findViewById(R.id.splash_video);
+        fadeView = findViewById(R.id.fade_view);
+
+        playIntro();
+    }
+
+    private void playIntro() {
+        Uri videoUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.intro);
+        videoView.setVideoURI(videoUri);
+
+        videoView.setOnPreparedListener(mp -> {
+            mp.setVideoScalingMode(MediaPlayer.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING);
+        });
+
+        videoView.setOnCompletionListener(mp -> startFadeOut());
+
+        videoView.setOnErrorListener((mp, what, extra) -> {
+            goToMain();
+            return true;
+        });
+
+        videoView.start();
+    }
+
+    private void startFadeOut() {
+        AlphaAnimation anim = new AlphaAnimation(0f, 1f);
+        anim.setDuration(800);
+        anim.setFillAfter(true);
+
+        fadeView.startAnimation(anim);
+
+        fadeView.postDelayed(this::goToMain, 100);
+    }
+
+    private void goToMain() {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        finish();
+    }
+}
